bed1858f026ee7888b77cb04d93e810f25918f7e
studymaterial9008-sudo <studymaterial9008@gmail.com>
2026-09-06 13:35:19 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB8L6LNPwE0nlysePWGt8oob7idJe9T",
-  authDomain: "jeevansetu-bcc74.firebaseapp.com",
-  projectId: "jeevansetu-bcc74",
-  storageBucket: "jeevansetu-bcc74.firebasestorage.app",
-  messagingSenderId: "485551494683",
-  appId: "1:485551494683:web:c94b4891cfc90f05e8abe",
-  measurementId: "G-D3VFGN7C3N"
-};
